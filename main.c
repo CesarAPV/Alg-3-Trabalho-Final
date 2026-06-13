620c1852c8dc6d630fac79f3053d96353219e3dc
Gustavo Henrique <silvagustavo@ufpr.br>
2026-06-13 11:54:40 -0300

    Comecei a função de iniciação da heap com as áreas calculadas.

--- main.c
+++ main.c
@@ -117,6 +117,48 @@ int id_Heap_min(Heap *h){
     return aux->id;
 }
 
+
+// - Da para usar um ponteiro para função aqui, por causa do 'tipo'
+// - Precisa corrigir a operação lógica, pois estou calculando a área
+// com base no tipo, porém não estou inserindo/construindo a heap
+// simultaneamente
+void calcula_area_for_heap_array(Heap *h, No *vet, int tipo){
+
+    float x1, y1, x2, y2, x3, y3;
+    if (tipo = 'a'){
+        h->array[0].id = &vet[0].id;
+        h->array[0].key = vet[0].key;
+        for(int i = 1; i < h->size; i ++){
+            x1 = vet[i-1].id;
+            y1 = vet[i-1].key;
+            x2 = vet[i].id;
+            y2 = vet[i].key;   
+            x3 = vet[i+1].id;
+            y3 = vet[i+1].key;
+            h->array[i].id = &vet[i].id;
+            h->array[i].key = area_triangulo(x1,y1,x2,y2,x3,y3);        
+        }
+        h->array[h->size--].id = &vet[h->size--].id;
+        h->array[h->size--].key = vet[h->size--].key;
+    }
+    else{
+        h->array[0].id = &vet[0].id;
+        h->array[0].key = vet[0].key;
+        for(int i = 1; i < h->size; i ++){
+            x1 = vet[i-1].id;
+            y1 = vet[i-1].key;
+            x2 = vet[i].id;
+            y2 = vet[i].key;   
+            x3 = vet[i+1].id;
+            y3 = vet[i+1].key;
+            h->array[i].id = &vet[i].id;
+            h->array[i].key = altura_triangulo(x1,y1,x2,y2,x3,y3);        
+        }
+        h->array[h->size--].id = &vet[h->size--].id;
+        h->array[h->size--].key = vet[h->size--].key;
+    }
+}
+
 // Função principal do programa
 // Pede o erro_base a ser retirado;
 // Retira o no min_heap se for menor que o erro;
@@ -124,7 +166,6 @@ int id_Heap_min(Heap *h){
 //...->tanto a direita quanto a esquerda do no a ser substituído;
 void process_execute(Heap *h, float erro, int tipo){
     
-    int controle_id;
     
 }
 
@@ -143,45 +184,43 @@ int main(int argc, char *argv[]) {
 
     scanf("%d\n", &tam_entrada);
 
-    //No *vet = malloc(sizeof(No) * tam_entrada);
+    No *vet = malloc(sizeof(No) * tam_entrada);
     Heap *h = create_heap(tam_entrada);
 
-    /* Os nós estão sendo inseridos no array auxiliar da estrura heap
-       depois da leitura, na função de execução, cria o heap com as áreas
-    */
-
     No d1  = {1,  15.5 };
-    //[d1.id--] = d1;
-    h->array[0] = d1;
+    vet[d1.id--] = d1;
+    //h->array[0] = d1;
     No d2  = {2,  10.3 };
-    //vet[d2.id--] = d2;
-    h->array[1] = d2;
+    vet[d2.id--] = d2;
+    //h->array[1] = d2;
     No d3  = {3,  40.3 };
-    //vet[d3.id--] = d3;
-    h->array[2] = d3;
+    vet[d3.id--] = d3;
+    //h->array[2] = d3;
     No d4  = {4,  5.6  };
-    //vet[d4.id--] = d4;
-    h->array[3] = d4;
+    vet[d4.id--] = d4;
+    //h->array[3] = d4;
     No d5  = {5,  231.2};
-    //vet[d5.id--] = d5;
-    h->array[4] = d5;
+    vet[d5.id--] = d5;
+    //h->array[4] = d5;
     No d6  = {6,  1.2  };
-    //vet[d6.id--] = d6;
-    h->array[5] = d6;
+    vet[d6.id--] = d6;
+    //h->array[5] = d6;
     No d7  = {7,  23.1 };
-    //vet[d7.id--] = d7;
-    h->array[6] = d7;
+    vet[d7.id--] = d7;
+    //h->array[6] = d7;
     No d8  = {8,  0.2  };
-    //vet[d8.id--] = d8;
-    h->array[7] = d8;
+    vet[d8.id--] = d8;
+    //h->array[7] = d8;
     No d9  = {9,  4.3  };
-    //vet[d9.id--] = d9;
-    h->array[8] = d9;
+    vet[d9.id--] = d9;
+    //h->array[8] = d9;
     No d10 = {10, 2.1  };
-    //vet[d10.id--] = d10;
-    h->array[9] = d10;
+    vet[d10.id--] = d10;
+    //h->array[9] = d10;
 
+    calcula_area_for_heap_array(h, vet, tipo);
     process_execute(h,erro, tipo);
+
 
     insert(h, &d1);
     insert(h, &d2);
@@ -193,8 +232,6 @@ int main(int argc, char *argv[]) {
     insert(h, &d8);
     insert(h, &d9);
     insert(h, &d10);
-
-    process_execute(h,Erro);
 
     No aux = {0};
     while(h->size > 0) {
